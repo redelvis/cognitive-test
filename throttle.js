22af798598e429810a49bcb3a143328f4f89008f
Max <max@maxshvedov.ru>
2017-10-27 13:22:31 +0300

    add throttle timeouts

--- throttle.js
+++ throttle.js
@@ -10,3 +10,13 @@ let throttle = function(fn, ms) {
     }
   };
 };
+
+var foo = throttle(function(msg) {
+  console.log(msg);
+}, 5000);
+
+foo('test');
+setTimeout(() => foo('test 2'), 0);
+setTimeout(() => foo('test 3'), 3000);
+setTimeout(() => foo('test 4'), 7500);
+setTimeout(() => foo('test 5'), 14000);
